11f7abdfe6817a9df10f6391fd44d103a9cd7fb4
D7004 <danicraftbr@hotmail.com>
2025-09-01 19:44:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ document.addEventListener('DOMContentLoaded', function(){
     accessButt.addEventListener('click', function (){
         accessButt.classList.toggle('rotation-butt');
         accessOpt.classList.toggle('show-list');
+
+        const buttSelect = accessButt.getAttribute('aria-expanded') === 'true';
+        accessButt.setAttribute('aria-expanded', !buttSelect)
     })
 
     const biggerFontButt = document.getElementById('bigger_font');
     const smallerFontButt = document.getElementById('smaller_font');
+
+    const diffContrast = document.getElementById('contrasting');
 
     let currentSize = 1;
 
@@ -21,4 +26,13 @@ document.addEventListener('DOMContentLoaded', function(){
         currentSize -= 0.1;
         document.body.style.fontSize = `${currentSize}rem`
     })
+
+    diffContrast.addEventListener('click', function(){
+        document.body.classList.toggle('contrasting');
+    })
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#wxs', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
